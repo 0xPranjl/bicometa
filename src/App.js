9bc9958c4433a25c05421be4c8bc86f5ebfc5ced
Pranjal402 <Pranjal.maurya402@gmail.com>
2022-06-29 15:55:29 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { ethers } from 'ethers';
+import Web3 from 'web3';
+import { useEffect, useState } from 'react';
+import {Biconomy} from "@biconomy/mexa";
+import abi from './abi';
 function App() {
+  const {ethereum}=window;
+  const [add,setadd]=useState();
+  const [c,setc]=useState("false");
+  
+  useEffect(()=>{
+    const biconomy = new Biconomy(window.ethereum,
+      {walletProvider:window.ethereum, apiKey:"UEf9ChbPv.a4a6d9bb-35b6-4300-9ade-d42ff09d2db4", debug: true});
+    //let ethersProvider = new ethers.providers.Web3Provider(biconomy);
+    let web3 = new Web3(biconomy);
+    biconomy.onEvent(biconomy.READY, () => {
+      //console.log("biconomy connected");
+      ethereum.request({ method: "eth_requestAccounts" }).then((e) => {
+        setadd(e[0]);
+        if(c=="true"){
+        u();
+        setc("false");
+        }
+      //  console.log(e[0])
+        //Session.set("add", e[0]);
+      });
+      // Initialize your dapp here like getting user accounts etc
+    }).onEvent(biconomy.ERROR, (error, message) => {
+     // console.log("biconomy error");
+      // Handle error while initializing mexa
+    });
+    async function u(){  
+      console.log("u ke andar");
+      let contract = new web3.eth.Contract(
+        abi,
+       "0x9b6afcddaf200bf4bc4d9df1213a6a11da8a1731"
+      );
+    let userAddress = add;
+   //all strings params must change as our smartcontract do not allow key duplication
+  let tx = contract.methods.createid("string1","string2","string3","nishu").send({
+    from: add
+    //signatureType: biconomy.PERSONAL_SIGN
+    //by default mexa will consider personal sign hence signatureType is optional 
+    //and can be omitted in case of personal sign
+});
+
+tx.on("transactionHash", function (hash) {
+    console.log(`Transaction hash is ${hash}`);
+    //showInfoMessage(`Transaction sent. Waiting for confirmation ..`);
+}).once("confirmation", function (confirmationNumber, receipt) {
+    console.log(receipt);
+    console.log(receipt.transactionHash);
+    //do something with transaction hash
+});
+    }
+  
+  },[c]
+);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+        <button onClick={()=>{
+       setc("true");
+        }}>See meta</button>
+        </div>
   );
 }
 
---
+++ src/abi.js
@@ -0,0 +1,284 @@
+const abi=[
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_trustedForwarder",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "m",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "Linkedwallets",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "id",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "value",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "walletname",
+				"type": "string"
+			}
+		],
+		"name": "addwallet",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "id",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_walletname",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "_ethadd",
+				"type": "address"
+			}
+		],
+		"name": "changeprimary",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "id",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "value",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "walletname",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_ucpisecretkey",
+				"type": "string"
+			}
+		],
+		"name": "createid",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "idexist",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "forwarder",
+				"type": "address"
+			}
+		],
+		"name": "isTrustedForwarder",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "mainid",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "numofwallet",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "primarywallet",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "trustedForwarder",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "ucpisecretkey",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "versionRecipient",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "wallet",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "walletowner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+module.exports=abi;
